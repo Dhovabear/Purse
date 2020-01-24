483065cd680a97a07a932affffd8fe14e3114665
Kilian <powerkiki25000@gmail.com>
2020-01-24 11:10:21 +0100

    Initial commit

---
+++ src/izly/CodeSecret.java
@@ -0,0 +1,43 @@
+package izly;
+
+import java.util.Random;
+
+public class CodeSecret {
+    private final String code;
+    private boolean codeDejaRevele;
+    private Random random;
+    private int nbEssaisRestants;
+
+    public CodeSecret(Random random) {
+        this.random = random;
+        codeDejaRevele = false;
+        code = createCode(random);
+        nbEssaisRestants = 3;
+    }
+
+    private String createCode(Random random) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            builder.append(random.nextInt(10));
+        }
+        return builder.toString();
+    }
+
+    public String revelerCode() {
+        if (!codeDejaRevele) {
+            codeDejaRevele = true;
+            return code;
+        }
+        return "xxxx";
+    }
+
+    public boolean verifierCode(String codeProposé) throws CodeBloquéException {
+        if (nbEssaisRestants <= 0) throw new CodeBloquéException();
+        if (!code.equals(codeProposé)){
+            nbEssaisRestants--;
+            return false;
+        }
+        nbEssaisRestants=3;
+        return true;
+    }
+}
---
+++ src/izly/DepassementPlafondException.java
@@ -0,0 +1,4 @@
+package izly;
+
+public class DepassementPlafondException extends Exception {
+}
